e69e8caa1371a6728c82f55ebdd7c317bb0a81e6
Pascal Thormeier <pascal.thormeier@liip.ch>
2020-04-05 15:20:16 +0200

    Add license page and bottom links on landing page

---
+++ pages/license.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="page">
+    <h1 class="bold">License</h1>
+    <p>
+      Attribution-NonCommercial 4.0 International (CC BY-NC 4.0), see
+      <a href="https://creativecommons.org/licenses/by-nc/4.0/" class="link">
+        createcommons.org
+      </a>
+      for details.
+    </p>
+    <nuxt-link to="/" tag="a" class="link">
+      Back
+    </nuxt-link>
+  </div>
+</template>
